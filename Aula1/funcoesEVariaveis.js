c6c9e762325a4c8643a929ec009d90df7ecee138
Anthony-UetaVecchia <tony.ueta@gmail.com>
2023-10-16 22:27:37 -0300

    Primeira aula de JavaScript

---
+++ Aula1/funcoesEVariaveis.js
@@ -0,0 +1,13 @@
+var variavel1 = 'Jagunço'
+let variavel2 = 3
+const pessoa = {
+  nome: "Anthony",
+  signo: "Cancêr",
+  idade: 17
+}
+
+console.log("Salve Mundo!");
+console.log(variavel1 + 'Ctrl+S');
+console.log(variavel1, 'Ctrl+S');
+// console.log(variavel1, variavel2, variavel3);
+console.log(pessoa);
